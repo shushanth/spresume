bfd998fab66793f13fbddb4c8a7b992bb28f00a2
Shushanth Pallegar <shushanth@sysops-MBP.fritz.box>
2019-12-08 22:13:56 +0100

    logging the proper client ip details

--- server/index.js
+++ server/index.js
@@ -5,10 +5,9 @@ const getClientIp = require('ipware')().get_ip;
 app = express();
 const clientBuildPath = path.resolve(__dirname, '..', 'dist');
 app.use((req, res, next) => {
-    console.log(req.connection.remoteAddress)
+    console.log('remoteAddress',req.connection.remoteAddress)
     const clientIpInfo = getClientIp(req);
-    console.log(clientIpInfo);
-    console.log(req.get('host'));
+    console.log('clientIpInfo', clientIpInfo);
     next();
 })
 app.use(serveStatic(clientBuildPath));
