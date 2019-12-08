c0af9ece72467a2e981ab426e5d476cdf6b4cc9d
Shushanth Pallegar <shushanth@sysops-MBP.fritz.box>
2019-12-08 22:35:40 +0100

    Added host name functionality

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const dns = require('dns');
 const serveStatic = require('serve-static');
 const getClientIp = require('ipware')().get_ip;
 app = express();
@@ -8,9 +7,7 @@ const clientBuildPath = path.resolve(__dirname, '..', 'dist');
 app.use((req, res, next) => {
     const remoteAddress = req.connection.remoteAddress;
     const clientIpInfo = getClientIp(req);
-    dns.reverse(remoteAddress, (err, domains) => {
-        console.log(domains);
-    })
+    console.log('hostName', req.headers.host);
     console.log('remoteAddress',remoteAddress)
     console.log('clientIpInfo', clientIpInfo);
     next();
