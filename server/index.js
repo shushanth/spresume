6cd6a2b72fbbde8691df6f9fa48d13c947b9fd10
Shushanth Pallegar <shushanth@sysops-MBP.fritz.box>
2019-12-08 22:04:58 +0100

    Added clients request ip information

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+const getClientIp = require('ipware')().get_ip;
+app = express();
+const clientBuildPath = path.resolve(__dirname, '..', 'dist');
+app.use((req, res, next) => {
+    console.log(req.connection.remoteAddress)
+    const clientIpInfo = getClientIp(req);
+    console.log(clientIpInfo);
+    console.log(req.get('host'));
+    next();
+})
+app.use(serveStatic(clientBuildPath));
+const port = process.env.PORT || 3100;
+app.listen(port);
+console.log('server started '+ port);
