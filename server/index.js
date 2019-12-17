5a865c7d9dcaea767e30a792b0ddb352dce58309
Shushanth Pallegar <shushanth@sysops-MBP.fritz.box>
2019-12-17 20:35:11 +0100

    changed professionaly summary

--- server/index.js
+++ server/index.js
@@ -7,6 +7,15 @@ const expressip = require('express-ip');
 
 const app = express();
 const clientBuildPath = path.resolve(__dirname, '..', 'dist');
+app.use((req, res, next) => {
+    const remoteAddress = req.connection.remoteAddress;
+    const clientIpInfo = getClientIp(req);
+    console.log('hostName', req.headers.host);
+    console.log('remoteAddress',remoteAddress)
+    console.log('clientIpInfo', clientIpInfo);
+    next();
+})
+app.use(serveStatic(clientBuildPath));
 const port = process.env.PORT || 3100;
 
 //middlewares
